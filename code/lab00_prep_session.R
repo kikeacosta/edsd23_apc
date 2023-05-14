de84330dd0a8b196c5018a504113f21d2824fe92
Enrique Acosta <kikepaila@gmail.com>
2023-05-14 12:03:47 +0200

    first commit

---
+++ code/lab00_prep_session.R
@@ -0,0 +1,383 @@
+# EDSD 2023
+# Course in analysis of mortality disturbances 
+# Instructor: Enrique Acosta (CED)
+# Preparing environment
+
+# installing missing packages ====
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+libs <- c("devtools",
+          "tidyverse", 
+          "remotes",
+          "readr",
+          "haven", 
+          "viridisLite", 
+          "viridis", 
+          "mgcv",
+          "HMDHFDplus",
+          "ISOweek",
+          "patchwork",
+          "RColorBrewer")
+
+for (i in libs){
+  if (!(i %in% rownames(installed.packages()))) install.packages(i)
+}
+options(timeout = 600)
+if (!("MortalitySmooth" %in% rownames(installed.packages()))) remotes::install_github("timriffe/MortalitySmooth")
+if (!("wpp2022" %in% rownames(installed.packages()))) remotes::install_github("PPgp/wpp2022", force = TRUE)
+
+# Loading required packages 
+lapply(libs, require, character.only = T)
+library("MortalitySmooth")
+library("wpp2022")
+
+# avoiding scientific notation
+options(scipen=999)
+# let's keep the same seed for reproducibility of results
+set.seed(2019) 
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# population data from WPP 2022 ====
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# loading population data from WPP 2022 estimates
+
+if (!file.exists("data_input/wpp2022_pop.rds")) {
+  
+  data(pop1dt)
+  data(popproj1dt)
+  
+  pop_hist <- 
+    pop1dt %>% 
+    as_tibble() %>% 
+    # filter(name =='Spain') %>% 
+    select(name, year, pop, popM, popF)
+  
+  pop_proj <- 
+    popproj1dt %>% 
+    as_tibble() %>% 
+    # filter(name =='Spain') %>% 
+    select(name, year, pop, popM, popF)
+  
+  pop <- 
+    bind_rows(pop_hist, pop_proj) %>% 
+    arrange(name, year)
+  
+  # write_rds(pop, "data_input/total_annual_population_all_countries.rds")
+  write_rds(pop, "data_input/wpp2022_pop.rds")
+  
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Deaths and exposures from the HMD ====
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+if (!file.exists("data_input/hmd_dts_pop.rds")) {
+ 
+  # HMD user and password
+  # usethis::edit_r_environ()
+  # hmd_us="acosta@demogr.mpg.de"
+  # hmd_pw="Secreto_1"
+  
+  # getting HMD username and password from the R environment
+  hmd_us <- Sys.getenv("hmd_us")
+  hmd_pw <- Sys.getenv("hmd_pw")
+  
+  cds_hmd <- getHMDcountries() %>% pull(CNTRY)
+  
+  hmd <- tibble()
+  for(ct in cds_hmd){
+    cat(paste0(ct, "\n"))
+    chunk_d <- 
+      readHMDweb(ct, "Deaths_1x1", hmd_us, hmd_pw) %>%
+      as_tibble() %>%
+      mutate(Code = ct)
+    
+    hmd <- 
+      hmd %>%
+      bind_rows(chunk_d)
+  }
+  
+  hmd
+  
+  hmd_e <- tibble()
+  for(ct in cds_hmd){
+    cat(paste0(ct, "\n"))
+    chunk_e <- 
+      readHMDweb(ct, "Exposures_1x1", hmd_us, hmd_pw) %>%
+      as_tibble() %>%
+      mutate(Code = ct)
+    
+    hmd_e <- 
+      hmd_e %>%
+      bind_rows(chunk_e)
+  }
+  
+  hmd2 <- 
+    hmd %>% 
+    rename_all(tolower) %>% 
+    select(-openinterval) %>% 
+    gather(female, male, total, key = sex, value = dts)
+  
+  hmd_e2 <- 
+    hmd_e %>% 
+    rename_all(tolower) %>% 
+    select(-openinterval) %>% 
+    gather(female, male, total, key = sex, value = pop)
+  
+  hmd_all <- 
+    hmd2 %>% 
+    left_join(hmd_e2) 
+  
+  write_rds(hmd_all, "data_input/hmd_dts_pop.rds")
+  
+}
+
+# other HMD data?
+# New Zealand Maories and non Maories 
+# Germany East and West
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Age-specific fertility rates from the HFD ====
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+if (!file.exists("data_input/hfd_asfr.rds")) {
+  
+  hmd_us <- Sys.getenv("hmd_us")
+  hmd_pw <- Sys.getenv("hmd_pw")
+  
+  # some countries having issues when trying to download... 
+  # excluding them for now
+  cts_issues <- c("BGR", "CAN", "POL", "KOR", "RUS", "CHE")
+  
+  cds_hfd <- 
+    getHFDcountries() %>% 
+    filter(!CNTRY %in% cts_issues) %>% 
+    pull(CNTRY)
+  
+  hfd <- tibble()
+  for(ct in cds_hfd[c(1:3, 6:43)]){
+    cat(paste0(ct, "\n"))
+    
+    chunk_f <- 
+        readHFDweb(ct, "asfrRR", hmd_us, hmd_pw) %>%
+        as_tibble() %>%
+        mutate(Code = ct)
+    
+    hfd <- 
+      hfd %>%
+      bind_rows(chunk_f) %>% 
+      unique()
+  }
+  hfd
+  
+  hfd2 <- 
+    hfd %>% 
+    rename_all(tolower) %>% 
+    select(-openinterval)
+  
+  write_rds(hfd2, "data_input/hfd_asfr.rds")
+  
+}
+
+if (!file.exists("data_input/hfd_parity.rds")) {
+  
+  cts_par <- c("ESP", "SWE", "JPN", "ITA", "DEUTE", "DEUTW")
+  
+  hfd_par <- tibble()
+  for(ct in cts_par){
+    cat(paste0(ct, "\n"))
+    
+    chunk_p <- 
+      readHFDweb(ct, "asfrRRbo", hmd_us, hmd_pw) %>%
+      as_tibble() %>%
+      mutate(Code = ct)
+    
+    hfd_par <- 
+      hfd_par %>%
+      bind_rows(chunk_p) %>% 
+      unique()
+  }
+  hfd_par
+  
+  hfd_par2 <- 
+    hfd_par %>% 
+    select(-OpenInterval)
+  
+  write_rds(hfd_par2, "data_input/hfd_parity.rds")
+  
+}
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# redefining the Lexis shape specs ====
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+redo_lexis_shape <- 
+  function(pmin, pmax, amin, amax){
+    lexis_shape <<-  
+      list(
+        geom_vline(xintercept = seq(pmin, pmax, 10), 
+                   linewidth = 0.2, linetype = "dashed", 
+                   alpha = 0.8, color = "grey30"),
+        geom_hline(yintercept = seq(amin, amax, 10), 
+                   linewidth = 0.2, linetype = "dashed", 
+                   alpha = 0.8, color = "grey30"),
+        # adding cohorts
+        geom_abline(intercept = seq(-pmax, -(pmin-amax), 10), slope = 1, 
+                    linetype = "dashed", color = "grey30", 
+                    linewidth = .2, alpha = 0.8),
+        coord_equal(expand = 0),
+        # adding proper labels to both axis
+        scale_x_continuous(breaks = seq(pmin, pmax, 10)),
+        scale_y_continuous(breaks = seq(amin, amax, 10)),     
+        # adding axis titles
+        labs(y = "Age", x = "Period"),
+        theme_bw()
+      )
+  }
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# plot a Lexis surface of mortality change over time ====
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+plot_change <- function(c, s, amin, amax, ymin, ymax){
+  # filtering data, and computing rates and log_rates 
+  db2 <- db %>% 
+    mutate(deaths = deaths + 1,
+           Mx = 100000 * deaths / exposure, 
+           log_m = log(Mx)) %>% 
+    filter(country == c,
+           sex == s,
+           age >= amin & age <= amax, 
+           year >= ymin & year <= ymax)
+  
+  amin2 <- db2 %>% pull(age) %>% min()
+  amax2 <- db2 %>% pull(age) %>% max()
+  ymin2 <- db2 %>% pull(year) %>% min()
+  ymax2 <- db2 %>% pull(year) %>% max()
+  
+  ylist <- unique(db2$year) %>% sort() # all periods in data, ordered
+  alist <- unique(db2$age) %>% sort() # all ages in data, ordered
+  
+  # mortality
+  deaths <- 
+    matrix(db2$deaths, nrow = length(alist), ncol = length(ylist), byrow = F)
+  colnames(deaths) <- ylist
+  rownames(deaths) <- alist
+  
+  # population at risk (population/exposure)
+  exposure <- 
+    matrix(db2$exposure, nrow = length(alist), ncol = length(ylist), byrow=F)
+  colnames(exposure) <- ylist
+  rownames(exposure) <- alist
+  
+  # smoothing mortality with best AIC (that is method=2, also possible best BIC with method=1)
+  fit <- 
+    Mort2Dsmooth(x = alist, y = ylist, Z = deaths, offset = log(exposure),
+                 overdispersion = TRUE, method = 2)
+  
+  # transforming them to smoothed deaths
+  mx_smooth <- (exp(fit$logmortality) * 100000)
+  
+  # from matrix to tidy form (adjusting ages to start in 0)
+  smt <- 
+    mx_smooth %>% 
+    as_tibble() %>% 
+    rownames_to_column() %>% 
+    rename(age = rowname) %>% 
+    gather(-age, key = year, value = Mx) %>% 
+    mutate(type = "m_smoothed",
+           age = as.integer(age) - 1 + amin2,
+           year = as.integer(year))
+  
+  # replacing missing values and estimating log_rates (/100k)
+  smt2 <- 
+    smt %>% 
+    replace_na(list(Mx = 0)) %>% 
+    mutate(log_m = log(Mx))
+  
+  db_per <- 
+    smt2 %>% 
+    group_by(age) %>% 
+    mutate(ch = ((Mx / lag(Mx)) - 1) * 100) %>% 
+    ungroup() %>% 
+    drop_na()
+  
+  qt <- 25
+  # one for decrease of mortality (from green to blue), 
+  # another for mortality increases (from yellow to red)
+  col_scale <- c(colorRampPalette(c("royalblue", "springgreen"), space = "Lab")(qt),
+                 colorRampPalette(c("yellow", "red"), space = "Lab")(qt))
+  
+  # Definition of brackets for the scale of change
+  val <- unique(db_per$ch)
+  # separate negative and positive values
+  pval <- val[val>0] # all positive values of change (mortality deterioration)
+  nval <- val[val<0] # all negative values of change (mortality improvement)
+  # identification of brackets for positive values (minimum + 23 quantiles + maximum)
+  pcop <-c(min(pval), quantile(pval, prob=1/(qt-1)*(1:(qt-2))), max(pval)*1.01)
+  # the same as above but for negative values (minimum + 23 quantiles + maximum)
+  ncop <-c(min(nval)*1.01, quantile(nval, prob=1/(qt-1)*(1:(qt-2))), max(nval)*1.01) 
+  # chain of brackets 25 ranges for negative changes, central value of no change (0), 
+  # and 25 ranges for positive changes
+  breaks_mc <- c(ncop, 0, pcop) 
+  
+  # adding to each value of change (continuous) the corresponding bracket (a discrete interval)
+  db_per2 <- 
+    db_per %>% 
+    mutate(ch_cut = cut(db_per$ch, breaks = breaks_mc)) 
+  
+  # all categories
+  cuts <- db_per2 %>% pull(ch_cut) %>% unique() %>% sort()
+  
+  # assigning color to each category
+  col_values <- setNames(col_scale, cuts)
+  
+  # Plot of mortality change over periods
+  p_change <- 
+    db_per2 %>% 
+    ggplot(aes(year, age, z = ch_cut)) +
+    geom_tile(aes(fill = ch_cut))+
+    # adding the color palette constructed above
+    scale_fill_manual(values = col_values, 
+                      breaks = cuts[c(1, 10, 20, 25, 30, 40, 50)], 
+                      name = "Mortality\nchange %")+ 
+    #adding contour lines when the slope is 0
+    geom_contour(aes(z = ch), breaks = 0, col="black", alpha=.7, size=.3)+ 
+    scale_x_continuous(expand = c(0,0), breaks = seq(ymin2, ymax2, 10)) +
+    scale_y_continuous(expand = c(0,0), breaks = seq(amin2, amax2, 10))+
+    # adding the grid of the Lexis diagram
+    geom_vline(xintercept = seq(ymin2, ymax2, 10), linetype = "dashed", 
+               color = "grey30", size = .20, alpha = 0.8) +
+    geom_hline(yintercept = seq(amin2, amax2, 10), linetype = "dashed", 
+               color = "grey30", size = .20, alpha = 0.8) +
+    geom_abline(intercept = seq(-2020, -(ymin2-amax2), 10), slope = 1, 
+                linetype = "dashed", color = "grey30", size = .2, alpha = 0.8)+
+    labs(x="Period", y="Age")+
+    # aesthetic details
+    coord_equal() +
+    labs(title = paste0(c, "_", s),
+         x="Period", y="Age")+
+    theme_minimal()+
+    # deleting default discrete legend
+    theme(
+      axis.text.x = element_text(size = 8),
+      axis.text.y = element_text(size = 8),
+      axis.title.x = element_text(size = 10),
+      axis.title.y = element_text(size = 10),
+      legend.text = element_text(size = 8),
+      legend.title = element_text(size = 9),
+      plot.title = element_text(size = 12),
+      panel.background = element_rect(fill = NA),
+      panel.grid.major = element_line(colour = NA),
+      panel.grid.minor = element_line(colour = NA),
+      plot.background = element_rect(fill = "white", colour = "transparent")
+    )
+  
+  return(p_change)
+}
+
+
+
+
+
---
+++ code/lab01_intro_viz_mortality.R
@@ -0,0 +1,370 @@
+# EDSD 2023
+# course in analysis of mortality disturbances 
+# Instructor: Enrique Acosta (CED)
+# Lab 1: Introduction to mortality visualization
+
+rm(list=ls())
+source("code/lab00_prep_session.R")
+
+# 1. Colors ====
+# ~~~~~~~~~~~~~~
+
+# standard palettes
+# ~~~~~~~~~~~~~~~~~
+# a few packages make life easier, for instance "RColorBrewer"
+
+# display all available palettes
+# (sequential, qualitative, and divergent palettes)
+display.brewer.all()
+
+# what about color-blind friendly palettes
+display.brewer.all(colorblindFriendly = TRUE)
+
+# choosing a different number of colors
+display.brewer.pal(5, "Set3")
+
+# show me the hexadecimal color codes 
+brewer.pal(5, "Set3")
+
+# other ideas
+# https://colorbrewer2.org/
+# https://coolors.co/
+# https://cran.r-project.org/web/packages/viridis/vignettes/intro-to-viridis.html
+
+
+# build our own palette
+# ~~~~~~~~~~~~~~~~~~~~~
+col1 <- "#0019FF"
+col2 <- "#FF0000"
+
+qt <- 5
+
+scale1 <- colorRampPalette(c(col1, col2), space = "Lab")(qt)
+scale1
+
+# 2. unit scales for plotting mortality ====
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# It is a good practice to use a log scale when plotting mortality rates 
+# at different ages, proportional changes, and ratios, among others...
+hmd <- read_rds("data_input/hmd_dts_pop.rds")
+head(hmd)
+
+ct <- "ESP"
+
+dt <- 
+  hmd %>% filter(code == "ESP") %>% 
+  # estimating death rates and their log
+  mutate(mx = 1e5*dts/pop,
+         log_mx = log(mx)) %>% 
+  filter(age <= 100)
+
+# mortality rates
+# ~~~~~~~~~~~~~~~
+# plotting age-specific death rates between 2015 and 2020
+mxs <- 
+  dt %>% 
+  filter(year %in% 2015:2020) %>% 
+  ggplot()+
+  geom_line(aes(age, mx, col = factor(year)))+
+  facet_grid(~sex)+
+  scale_color_manual(values = c(scale1, "black"))+
+  theme_bw()
+
+mxs
+
+# log scale in the y axis
+dt %>% 
+  filter(year %in% 2015:2020) %>% 
+  ggplot()+
+  geom_line(aes(age, log_mx, col = factor(year)))+
+  facet_grid(~sex)+
+  scale_color_manual(values = c(scale1, "black"))+
+  theme_bw()
+
+# but how to interpret the values?
+
+# better to use the log scale keeping the original values
+# super easy in ggplot
+mxs+scale_y_log10()
+
+
+
+# 3. plotting estimates of multiplicative order ====
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# ratios
+# ~~~~~~
+sexr <- 
+  dt %>% 
+  filter(year == 2020) %>% 
+  select(age, sex, mx) %>% 
+  spread(sex, mx) %>% 
+  mutate(sexr = male/female,
+         log_sexr = log(sexr))
+
+p_sexr <- 
+  sexr %>% 
+  ggplot()+
+  geom_point(aes(age, sexr))+
+  # adding a line to identify the equality (ratio = 1)
+  geom_hline(yintercept = 1, linetype  = "dashed")+
+  theme_bw()
+
+p_sexr
+# any issues with this scale?
+# by principle, opposite values should be equidistant, and the two scales your 
+# are measuring should have the same magnitude
+# it is different the meaning of scale when our thinking is 
+# additive or multiplicative
+# e.g., in the case of ratios it is a multiplicative relation
+
+# in additive terms, the opposite of 2 is -2, the center is 0
+# in multiplicative terms? what is the opposite of double? what is the center?
+# what are the possible values in each direction?
+
+# a solution for this is to use log scales
+# the distance between 0.5 and 1 is the same as between 1 and 2
+# the distance between 0 and 1 is the same as between 1 and infinite
+# lets apply log scales
+p_log_sexr <- 
+  sexr %>% 
+  ggplot()+
+  geom_point(aes(age, log_sexr))+
+  # adding a line to identify the equality (ratio = 1)
+  geom_hline(yintercept = 1, linetype  = "dashed")+
+  theme_bw()
+
+p_log_sexr
+
+# what happened? how to interpret the values? even the reference is not in the 
+# proper place
+# again, use the ggplot option to use the log scales while keeping the actual 
+# labels
+p_sexr+scale_y_log10(breaks = c(0.5, 1, 2, 4))
+
+# the same principle works for relative risk (RR) measures!!
+
+
+
+
+# 4. Lexis diagrams ====
+# ~~~~~~~~~~~~~~~~~~~~~~
+# the main visualization tool for demographers!!
+
+amin <- 0
+amax <- 100
+pmin <- 1910
+pmax <- 2020
+
+p_lexis <- 
+  ggplot()+
+  # adding lines for periods
+  geom_vline(xintercept = seq(pmin, pmax, 10), 
+             linewidth = 0.2, linetype = "dashed", alpha = 0.8, color = "grey30")+
+  # adding lines for ages
+  geom_hline(yintercept = seq(amin, amax, 10), 
+             linewidth = 0.2, linetype = "dashed", alpha = 0.8, color = "grey30")+
+  theme_bw()
+  
+p_lexis
+
+# lets plot the life of someone who was born in 1960
+bt_coh <- 1960
+
+p_lexis+
+  geom_segment(aes(x = bt_coh, y = 0, xend = pmax, yend = pmax - bt_coh), 
+               colour = "red")
+
+# the age and period scales are different, and there is unnecessary extra space
+p_lexis+
+  geom_segment(aes(x = bt_coh, y = 0, xend = pmax, yend = pmax - bt_coh), 
+               colour = "red")+
+  # cutting extra space
+  coord_equal(expand = 0)
+
+
+# a few additional things
+p_lexis+
+  geom_segment(aes(x = bt_coh, y = 0, xend = pmax, yend = pmax - bt_coh), 
+               colour = "red")+
+  coord_equal(expand = 0)+
+  # adding proper labels to both axis
+  scale_x_continuous(breaks = seq(pmin, pmax, 10))+
+  scale_y_continuous(breaks = seq(amin, amax, 10))+
+  # adding axis titles
+  labs(y = "Age", x = "Period")+
+  # adding cohorts
+  geom_abline(intercept = seq(-pmax, -(pmin-amax), 10), slope = 1, 
+              linetype = "dashed", color = "grey30", linewidth = .2, alpha = 0.8)
+
+# saving preferences in an object
+# tip: we can create a list of customizations and then apply that to the plot
+lexis_shape <- 
+  list(
+    geom_vline(xintercept = seq(pmin, pmax, 10), 
+               linewidth = 0.2, linetype = "dashed", 
+               alpha = 0.8, color = "grey30"),
+    geom_hline(yintercept = seq(amin, amax, 10), 
+               linewidth = 0.2, linetype = "dashed", 
+               alpha = 0.8, color = "grey30"),
+    # adding cohorts
+    geom_abline(intercept = seq(-pmax, -(pmin-amax), 10), slope = 1, 
+                linetype = "dashed", color = "grey30", 
+                linewidth = .2, alpha = 0.8),
+    coord_equal(expand = 0),
+    # adding proper labels to both axis
+    scale_x_continuous(breaks = seq(pmin, pmax, 10)),
+    scale_y_continuous(breaks = seq(amin, amax, 10)),
+    # adding axis titles
+    labs(y = "Age", x = "Period"),
+    theme_bw()
+  )
+
+ggplot()+
+  lexis_shape
+
+redo_lexis_shape(pmin = 1970,
+                 pmax = 2010,
+                 amin = 20,
+                 amax = 80)
+
+ggplot()+
+  lexis_shape
+
+redo_lexis_shape(pmin,pmax,amin,amax)
+
+  
+# 5. Lexis surfaces ====
+# ~~~~~~~~~~~~~~~~~~~~~~
+# lets plot the mortality experience in Spain between 1950 and 2020, 
+# between ages 0 and 100
+dt2 <- 
+  dt %>% 
+  filter(year %in% pmin:pmax,
+         age %in% amin:amax)
+  
+# quick plot of log_rates
+dt2 %>%
+  ggplot(aes(x = year, y = age, z = mx))+
+  geom_tile(aes(fill = mx))
+
+# we can already apply what we used for the diagram
+dt2 %>%
+  ggplot(aes(x = year, y = age, z = mx))+
+  geom_tile(aes(fill = mx))+
+  lexis_shape
+
+# lets add a nice scale color, package viridis has some nice options
+# https://cran.r-project.org/web/packages/viridis/vignettes/intro-to-viridis.html
+library(viridis)
+
+dt2 %>%
+  ggplot(aes(x = year, y = age, z = mx))+
+  geom_tile(aes(fill = mx))+
+  # adding a viridis scale
+  scale_fill_viridis(option = "C", discrete = F,  direction = -1, 
+                     # breaks = brks, labels = lbls,
+                     name = "Mortality\nrate /100k") +
+  # contour lines are useful for looking at level changes over time
+  geom_contour(bins = 20, col = "black", size = .15, alpha = 0.8)+
+  lexis_shape
+
+# you can modify the option parameter for other nice palettes
+dt2 %>%
+  ggplot(aes(x = year, y = age, z = mx))+
+  geom_tile(aes(fill = mx))+
+  # adding a viridis scale
+  scale_fill_viridis(option = "F", discrete = F,  direction = -1, 
+                     # breaks = brks, labels = lbls,
+                     name = "Mortality\nrate /100k") +
+  # contour lines are useful for looking at level changes over time
+  geom_contour(bins = 20, col = "black", size = .15, alpha = 0.8)+
+  lexis_shape
+
+# what about the scales?
+# can we select the breaks? can we select the labels to show?
+brks <- quantile(c(min(dt2$mx), max(dt2$mx)), probs = seq(0, 1, 0.25))
+lbls <- round(brks)
+
+dt2 %>%
+  ggplot(aes(x = year, y = age, z = mx))+
+  geom_tile(aes(fill = mx))+
+  scale_fill_viridis(option = "C", discrete = F,  direction = -1, 
+                     breaks = brks, labels = lbls,
+                     name = "Mortality\nrate /100k") +
+  geom_contour(bins = 20, col = "black", size = .15, alpha = 0.8)+
+  lexis_shape
+
+# nice, but... what about the log rule for death rates?
+# lets use the log then
+
+dt2 %>%
+  ggplot(aes(x = year, y = age, z = log_mx))+
+  geom_tile(aes(fill = log_mx))+
+  scale_fill_viridis(option = "C", discrete = F,  direction = -1, 
+                     name = "Mortality\nrate /100k") +
+  geom_contour(bins = 20, col = "black", size = .15, alpha = 0.8)+
+  lexis_shape
+  
+  
+# are the scales better? how to interpret them?
+s_lexis_log+
+  scale_z_log10()
+
+# ups!!!!!
+
+# select a sample and convert it
+brks <- quantile(c(min(dt2$log_mx), max(dt2$log_mx)), probs = seq(0, 1, 0.25))
+lbls <- round(exp(brks))
+
+dt2 %>%
+  ggplot(aes(x = year, y = age, z = log_mx))+
+  geom_tile(aes(fill = log_mx))+
+  # adding axis titles
+  scale_fill_viridis(option = "C", discrete = F,  direction = -1, 
+                     breaks = brks, labels = lbls,
+                     name = "Mortality\nrate /100k") +
+  lexis_shape
+
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~
+# Assignment in class: ====
+# ~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# Option 1.
+# ~~~~~~~~~
+# Construct a lexis surface of the sex ratio of mortality in a country of 
+# your choice between 1900 and 2000
+dt
+
+# Option 2.
+# ~~~~~~~~~
+# Construct a lexis surface of age specific fertility rates ratios 
+# between two countries of your choice with available data for all available 
+# years
+asfr <- 
+  read_rds("data_input/hfd_asfr.rds") 
+asfr
+unique(asfr$code)
+
+
+# in both cases:
+# ~~~~~~~~~~~~~~
+# use the appropriate scales, labels, and colors that allows an easy and proper
+# interpretation of the ratios
+
+# add contour lines to distinguish the direction of the ratios
+# what kind of color scale is the most appropriate? 
+# build a color scale if there is no a good one available
+
+# add cohort labels to the Lexis surface
+
+# Is there anything to interpret?
+# Any additional analysis that can be done?
