45a823e1b81d6fda6d47d5db46cac917533a2232
Enrique Acosta <kikepaila@gmail.com>
2023-05-17 23:55:36 +0200

    updating session 4

--- code/lab00_prep_session.R
+++ code/lab00_prep_session.R
@@ -567,3 +567,282 @@ plot_carst <-
     p_a_eff+p_pc_eff+ 
       plot_layout(widths = c(1, 2.5))
   }
+
+
+
+cd = "FRA"; sx = "b"; ag = "TOT"; ymin = 2013
+
+
+obtain_excess <- 
+  function(cd, sx, ag, ymin){
+    
+    file_name <- zip_files %>% filter(str_detect(Name, cd)) %>% pull(Name)
+    
+    dt <- 
+      read_csv(unz("data_input/STMFinput.zip", file_name)) %>% 
+      mutate(Week = as.double(Week))
+    
+    # adding date to each ISO week, using the package ISOweek
+    # we need the week in this format: 2000-W01-7
+    dt2 <- 
+      dt %>% 
+      # renaming all variablees to lower case
+      rename_with(str_to_lower) %>% 
+      # adding the date
+      mutate(isoweek = paste0(year, "-W", sprintf("%02d", week), "-7"),
+             date = ISOweek2date(isoweek)) %>% 
+      filter(age == ag,
+             sex == sx,
+             year >= ymin) %>% 
+      select(code = popcode, year, week, dts = deaths, date)
+    
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # estimating the baseline mortality ====
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+    # 1. average-week
+    # ~~~~~~~~~~~~~~~
+    # the simplest approach (maybe the worst)
+    
+    # estimating the average weekly deaths for the whole training period
+    # as the baseline
+    w_av <- 
+      dt2 %>% 
+      filter(year <= 2019) %>%
+      summarise(bsn = mean(dts)) %>% 
+      pull(bsn)
+    
+    dt_w_av <- 
+      dt2 %>% 
+      mutate(bsn = w_av)
+    
+    
+    # 2. Week-specific average
+    # ~~~~~~~~~~~~~~~~~~~~~~~~
+    ws_av <- 
+      dt2 %>% 
+      filter(year <= 2019) %>%
+      group_by(week) %>% 
+      summarise(bsn = mean(dts)) %>% 
+      ungroup()
+    
+    dt_ws_av <- 
+      dt2 %>% 
+      left_join(ws_av)
+    
+    
+    
+    # 3. Excess mortality using a Poisson model
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+    # exposures
+    # ~~~~~~~~~
+    # issue: population counts only available annually
+    # solution: interpolation
+    
+    # loading total population counts from WPP 
+    pop <- read_rds("data_input/wpp2022_pop.rds")
+    
+    # selecting the Spanish population
+    pop2 <- 
+      pop %>% 
+      mutate(code = countrycode(name, origin = "country.name",
+                                destination = "iso3c")) %>% 
+      filter(code == cd)
+    
+    # from annual to weekly exposures
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # wee need weekly exposures between 2010 and 2023
+    # then, we can interpolate between 2009 and 2024
+    
+    # first, create a grid with all weeks that we need to populate with 
+    # weekly exposures
+    pop_empty <- 
+      # 52 weeks per year
+      expand_grid(year = 2009:2024, week = 1:52) %>% 
+      # adding extra weeks for leap years 2009, 2015, and 2020
+      bind_rows(tibble(year = c(2009, 2015, 2020), week = 53)) %>% 
+      # arrange it chronologically
+      arrange(year, week)
+    
+    # preparing data for interpolation
+    pop_inter <- 
+      pop_empty %>%  
+      # adding annual population in midyear to week 26 (the midyear week!)
+      left_join(pop2 %>%
+                  select(year, pop) %>% 
+                  mutate(week = 26)) %>% 
+      mutate(t = 1:n(), # creating a continuous variable for week sequence 
+             # transforming year-week to date, using ISOweek2date() function
+             isoweek = paste0(year, "-W", sprintf("%02d", week), "-7"),
+             date = ISOweek2date(isoweek))
+    
+    # extracting values for interpolation
+    xs <- pop_inter %>% drop_na() %>% pull(t) # weeks with data
+    ys <- pop_inter %>% drop_na() %>% pull(pop) # available pop data
+    ts <- pop_inter %>% pull(t) # all weeks in which we need estimates
+    
+    # smoothing using cubic splines
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    # the "spline()" function allows to interpolate by constraining the curve 
+    # to match the original data. In other words, it is strictly interpolation 
+    # rather than smoothing  
+    
+    # extracting predictions from the model
+    interpolated_pop <- spline(xs, ys, xout = ts)
+    
+    pop_inter2 <- 
+      pop_inter %>% 
+      mutate(pop2 = interpolated_pop$y)
+    
+    # visualizing annual values and weekly estimates
+    pop_inter2 %>% 
+      ggplot()+
+      geom_line(aes(date, pop2))+
+      geom_point(aes(date, pop), col = "red")+
+      theme_bw()
+    
+    # weekly exposures to use
+    pop3 <- 
+      pop_inter2 %>% 
+      select(-pop, -t, -isoweek) %>% 
+      rename(pop = pop2)
+    
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+    # merging deaths and exposures
+    dt3 <- 
+      dt2 %>% 
+      left_join(pop3) %>% # merging with weekly population
+      mutate(exposure = pop / 52, # exposure in person-weeks
+             t = 1:n(), # a variable for the secular trend 
+             w = ifelse(date <= "2020-03-15", 1, 0)) # a variable for the weights 
+    
+    
+    # GAM models allow us to include parametric and semiparametric terms together 
+    # the "mgcv" package is very convenient for working with GAM models
+    # https://cran.r-project.org/web/packages/mgcv/mgcv.pdf
+    
+    # fitting the model
+    gam_model <- 
+      gam(dts ~
+            # linear term (exponential outside Poisson) for the secular trend
+            t +
+            # cyclic spline term for the seasonal trend
+            s(week, bs = 'cp') + 
+            # controlling for population changes over time
+            offset(log(exposure)), 
+          # to avoid including the pandemic period in the baseline estimation
+          weights = w, 
+          data = dt3, 
+          # using a quasipoisson distribution to account for overdispersion
+          family = "quasipoisson") 
+    
+
+    # example for predicting estimates
+    bsn_poi <- predict(gam_model, newdata = dt3, type = "response", se.fit = TRUE)
+    
+    # obtaining estimates for the three models
+    bsn <- 
+      dt3 %>% 
+      mutate(bsn = bsn_poi$fit,
+             se = bsn_poi$se.fit,
+             ll = bsn - 1.96*se,
+             ul = bsn + 1.96*se)
+
+    # comparing excess estimation approaches ====
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    
+    # binding together the three estimates
+    
+    bsns <- 
+      bind_rows(dt_w_av %>% 
+                  select(year, week, date, dts, bsn) %>% 
+                  mutate(type = "weekly_average"),
+                dt_ws_av %>% 
+                  select(year, week, date, dts, bsn) %>% 
+                  mutate(type = "weekly_spc_average"),
+                bsn %>% 
+                  select(year, week, date, dts, bsn) %>% 
+                  mutate(type = "poisson_model"))
+    
+    # plotting the three baselines
+    p_bsns <- 
+      bsns %>% 
+      ggplot()+
+      geom_line(aes(date, dts), linewidth = 1)+
+      geom_line(aes(date, bsn, col = type), linewidth = 1)+
+      scale_color_manual(values = cols)+
+      theme_bw()
+    p_bsns
+    
+    excs <- 
+      bsns %>% 
+      mutate(exc = dts - bsn,
+             psc = dts / bsn) %>% 
+      filter(date >= "2020-03-15",
+             date <= "2022-12-31")
+    
+    # visualizing weekly excess deaths
+    excs %>% 
+      ggplot()+
+      geom_line(aes(date, exc, col = type), linewidth = 1)+
+      geom_hline(yintercept = 0, linetype = "dashed")+
+      scale_color_manual(values = cols)+
+      theme_bw()
+    
+    # visualizing cumulative excess
+    excs_cum <- 
+      excs %>% 
+      group_by(type) %>% 
+      mutate(exc_cum = cumsum(exc)) %>% 
+      ggplot()+
+      # geom_line(aes(date, exc))+
+      geom_line(aes(date, exc_cum, col = type))+
+      theme_bw()
+    
+    # obtaining annual excess
+    yr_exc <- 
+      excs %>% 
+      filter(date >= "2020-03-15",
+             date <= "2022-12-31") %>% 
+      group_by(year, type) %>% 
+      summarise(exc = sum(exc)) %>% 
+      ungroup()
+    
+    tots <- 
+      yr_exc %>% 
+      spread(type, exc) %>% 
+      summarise(poisson_model = sum(poisson_model),
+                weekly_average = sum(weekly_average),
+                weekly_spc_average = sum(weekly_spc_average),
+                year = "total")
+    
+    t1 <- round(tots$poisson_model)
+    t2 <- round(tots$weekly_average)
+    t3 <- round(tots$weekly_spc_average)
+    
+    d2 <- paste0(round((t2 / t1 - 1)*100, 1), "%")
+    d3 <- paste0(round((t3 / t1 - 1)*100, 1), "%")
+    
+    # plotting excess estimates by year
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    p_tots <- 
+      yr_exc %>% 
+      ggplot(aes(type, exc, fill = factor(year))) + 
+      geom_bar(position="stack", stat="identity")+
+      geom_text(aes(x = "poisson_model", y = 0), label = paste0(t1, "\nreference"), vjust = -1)+
+      geom_text(aes(x = "weekly_average", y = 0), label = paste0(t2, "\n", d2), vjust = -1)+
+      geom_text(aes(x = "weekly_spc_average", y = 0), label = paste0(t3, "\n", d3), vjust = -1)+
+      labs(fill = "year")+
+      coord_cartesian(expand = 0)+
+      theme_bw()
+    
+    
+    
+    out_f <- list(plot_bsns <- p_bsns,
+                  plot_tots <- p_tots,
+                  yr_exc <- yr_exc)
+    
+    return(out_f)
+  }
--- code/lab04_excess.R
+++ code/lab04_excess.R
@@ -4,9 +4,11 @@
 # Lab 4: Introduction to estimation of excess mortality
 
 rm(list=ls())
-library(tidyverse)
-library(lubridate)
-library(ISOweek)
+source("code/lab00_prep_session.R")
+
+# 3 baselines: 3 colors
+cols <- brewer.pal(3, "Dark2")
+
 
 # loading mortality data ====
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -17,44 +19,51 @@ library(ISOweek)
 # hint: input STMF is much better than the output because it comes in 
 # finer age groups resolution (usually 5-y)! 
 
-# download.file("https://www.mortality.org/File/GetDocument/Public/STMF/Inputs/STMFinput.zip", 
-#               "data_input/STMFinput.zip")
-
 # list of country codes in STMF
 zip_files <- unzip("data_input/STMFinput.zip", list = TRUE)
 
-# lets look at Spanish data
-cd <- "ESP"
-spain_file <- zip_files %>% filter(str_detect(Name, cd)) %>% pull(Name)
+zip_files
 
-esp_dt <- 
-  read_csv(unz("data_input/STMFinput.zip", spain_file)) %>% 
+# lets look at Spanish data, total sex, all ages, since 2015
+cd <- "ESP"
+sx <- "b"
+ag <- "TOT"
+ymin <- 2013
+
+# test <-
+#   obtain_excess(cd, sx, ag, ymin)
+# 
+# test[[1]]
+# test[[2]]
+# test[[3]] %>% spread(type, exc)
+
+file_name <- zip_files %>% filter(str_detect(Name, cd)) %>% pull(Name)
+
+dt <- 
+  read_csv(unz("data_input/STMFinput.zip", file_name)) %>% 
   mutate(Week = as.double(Week))
 
-esp_dt
-unique(esp_dt$Type)
-unique(esp_dt$Access)
-unique(esp_dt$Age)
+dt
 
 # adding date to each ISO week, using the package ISOweek
 # we need the week in this format: 2000-W01-7
-esp_dt2 <- 
-  esp_dt %>% 
+dt2 <- 
+  dt %>% 
+  # renaming all variablees to lower case
   rename_with(str_to_lower) %>% 
+  # adding the date
   mutate(isoweek = paste0(year, "-W", sprintf("%02d", week), "-7"),
          date = ISOweek2date(isoweek)) %>% 
-  filter(age == "TOT",
-         sex == "b",
-         year >= 2010) %>% 
-  select(popcode, year, week, dts = deaths, date)
+  filter(age == ag,
+         sex == sx,
+         year >= ymin) %>% 
+  select(code = popcode, year, week, dts = deaths, date)
 
-esp_dt2 %>% 
+dt2 %>% 
   ggplot()+
-  geom_line(aes(date, dts))+
+  geom_line(aes(date, dts), linewidth = 1)+
   theme_bw()
 
-ggsave("figures/weekly_mortality_spain.png",
-       w = 8, h = 2)
 
 
 # ######################################
@@ -62,89 +71,59 @@ ggsave("figures/weekly_mortality_spain.png",
 # ######################################
 
 
-# Yearly average-week ==== 
+# average-week ====
 # ~~~~~~~~~~~~~~~~~~~~~~~~
 # the simplest approach (maybe the worst)
 
-# estimating the average weekly deaths for the whole training period 
+# estimating the average weekly deaths for the whole training period
 # as the baseline
-yw_av <- 
-  esp_dt2 %>% 
+w_av <- 
+  dt2 %>% 
   filter(year <= 2019) %>%
   summarise(bsn = mean(dts)) %>% 
   pull(bsn)
 
-esp_yw_av <- 
-  esp_dt2 %>% 
-  mutate(bsn = yw_av)
-  
-esp_yw_av %>% 
+dt_w_av <- 
+  dt2 %>% 
+  mutate(bsn = w_av)
+
+dt_w_av %>% 
   ggplot()+
-  geom_line(aes(date, dts))+
-  geom_line(aes(date, bsn), col = "#8ac926")+
+  geom_line(aes(date, dts), linewidth = 1)+
+  geom_line(aes(date, bsn), linewidth = 1, col = cols[1])+
   geom_vline(xintercept = ymd("2020-03-15"), linetype = "dashed")+
   theme_bw()
 
-ggsave("figures/weekly_mortality_spain_year_av_week_bsn.png",
-       w = 8, h = 2)
-
-
 # Week-specific average ====
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~
-wk_av <- 
-  esp_dt2 %>% 
+ws_av <- 
+  dt2 %>% 
   filter(year <= 2019) %>%
   group_by(week) %>% 
   summarise(bsn = mean(dts)) %>% 
   ungroup()
 
-esp_wk_av <- 
-  esp_dt2 %>% 
-  left_join(wk_av)
+dt_ws_av <- 
+  dt2 %>% 
+  left_join(ws_av)
 
-esp_wk_av %>% 
+dt_ws_av %>% 
   ggplot()+
-  geom_line(aes(date, dts))+
-  geom_line(aes(date, bsn), col = "#1982c4")+
+  geom_line(aes(date, dts), linewidth = 1)+
+  geom_line(aes(date, bsn), linewidth = 1, col = cols[2])+
   geom_vline(xintercept = ymd("2020-03-15"), linetype = "dashed")+
   theme_bw()
 
-ggsave("figures/weekly_mortality_spain_av_week_bsn.png",
-       w = 8, h = 2)
-
 # it seems not bad at all!!
-
-# # let's estimate the excess
-# 
-# # weekly excess deaths
-# exc_av <- 
-#   esp_wk_av %>% 
-#   filter(date >= "2020-03-01") %>% 
-#   mutate(exc = dts-bsn)
-# 
-# exc_av %>% 
-#   ggplot()+
-#   geom_line(aes(date, exc))+
-#   geom_hline(yintercept = 0, linetype = "dashed")+
-#   theme_bw()
-# 
-# # estimating total excess by year
-# tot_exc_av <- 
-#   exc_av %>% 
-#   group_by(year) %>% 
-#   summarise(dts = sum(dts),
-#             bsn = sum(bsn),
-#             exc = sum(exc)) %>% 
-#   ungroup() %>% 
-#   mutate(psc = dts / bsn - 1)
 
 
 # Excess mortality using a Poisson model ====
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+# we need: weekly deaths and exposures
+
 # exposures
 # ~~~~~~~~~
-# we need: weekly deaths and exposures
 # issue: population counts only available annually
 # solution: interpolation
 
@@ -152,16 +131,19 @@ ggsave("figures/weekly_mortality_spain_av_week_bsn.png",
 pop <- read_rds("data_input/wpp2022_pop.rds")
 
 # selecting the Spanish population
-esp_pop <- 
+pop2 <- 
   pop %>% 
-  filter(name == "Spain")
-  
+  filter(code == cd) %>%
+  select(year, pop) %>% 
+  mutate(week = 26)
+
 # from annual to weekly exposures
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # wee need weekly exposures between 2010 and 2023
 # then, we can interpolate between 2009 and 2024
 
-# first, create a grid with empty values
+# first, create a grid with all weeks that we need to populate with 
+# weekly exposures
 pop_empty <- 
   # 52 weeks per year
   expand_grid(year = 2009:2024, week = 1:52) %>% 
@@ -170,12 +152,13 @@ pop_empty <-
   # arrange it chronologically
   arrange(year, week)
 
+# preparing data for interpolation
 pop_inter <- 
   pop_empty %>%  
-  left_join(esp_pop %>%
-              select(year, pop) %>% 
-              mutate(week = 26)) %>% 
-  mutate(t = 1:n(),
+  # adding annual population in midyear to week 26 (the midyear week!)
+  left_join(pop2) %>% 
+  mutate(t = 1:n(), # creating a continuous variable for week sequence 
+         # transforming year-week to date, using ISOweek2date() function
          isoweek = paste0(year, "-W", sprintf("%02d", week), "-7"),
          date = ISOweek2date(isoweek))
 
@@ -190,19 +173,22 @@ ts <- pop_inter %>% pull(t) # all weeks in which we need estimates
 # to match the original data. In other words, it is strictly interpolation 
 # rather than smoothing  
 
+# extracting predictions from the model
+interpolated_pop <- spline(xs, ys, xout = ts)
+
 pop_inter2 <- 
   pop_inter %>% 
-  mutate(pop2 = spline(xs, ys, xout = ts)$y)
+  mutate(pop2 = interpolated_pop$y)
 
 # visualizing annual values and weekly estimates
 pop_inter2 %>% 
   ggplot()+
-  geom_line(aes(date, pop2))+
-  geom_point(aes(date, pop), col = "red")+
+  geom_line(aes(date, pop2), linewidth = 1)+
+  geom_point(aes(date, pop), col = "red", size = 3)+
   theme_bw()
 
 # weekly exposures to use
-pop2 <- 
+pop3 <- 
   pop_inter2 %>% 
   select(-pop, -t, -isoweek) %>% 
   rename(pop = pop2)
@@ -210,19 +196,17 @@ pop2 <-
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # merging deaths and exposures
-esp_dt3 <- 
-  esp_dt2 %>% 
-  left_join(pop2) %>% 
+dt3 <- 
+  dt2 %>% 
+  left_join(pop3) %>% # merging with weekly population
   mutate(exposure = pop / 52, # exposure in person-weeks
          t = 1:n(), # a variable for the secular trend 
-         w = ifelse(date <= "2020-03-15", 1, 0)) # a variable for the weights 
+         w = ifelse(date <= "2020-03-01", 1, 0)) # a variable for the weights 
 
 
 # GAM models allow us to include parametric and semiparametric terms together 
 # the "mgcv" package is very convenient for working with GAM models
 # https://cran.r-project.org/web/packages/mgcv/mgcv.pdf
-
-library(mgcv)
 
 # fitting the model
 gam_model <- 
@@ -235,7 +219,7 @@ gam_model <-
         offset(log(exposure)), 
       # to avoid including the pandemic period in the baseline estimation
       weights = w, 
-      data = esp_dt3, 
+      data = dt3, 
       # using a quasipoisson distribution to account for overdispersion
       family = "quasipoisson") 
 
@@ -247,76 +231,29 @@ summary(gam_model)
 ((1 + 0.00018984)^52)^10
 
 # example for predicting estimates
-predict(gam_model, newdata = esp_dt3, type = "response", se.fit = TRUE)
-predict(gam_model, newdata = esp_dt3)
+bsn_poi <- predict(gam_model, newdata = dt3, type = "response", se.fit = TRUE)
 
 # obtaining estimates for the three models
-esp_bsn <- 
-  esp_dt3 %>% 
-  mutate(bsn = predict(gam_model, 
-                       type = "response"),
-         se = predict(gam_model, 
-                      newdata = esp_dt3, 
-                      type = "response", 
-                      se.fit = TRUE)$se.fit,
+bsn <- 
+  dt3 %>% 
+  mutate(bsn = bsn_poi$fit,
+         se = bsn_poi$se.fit,
          ll = bsn - 1.96*se,
          ul = bsn + 1.96*se)
 
-esp_bsn %>% 
+bsn %>% 
   ggplot()+
-  geom_ribbon(aes(date, ymin = ll, ymax = ul), alpha = 0.5, fill = "red")+
-  geom_line(aes(date, dts))+
-  geom_line(aes(date, bsn), col = "red")+
+  geom_ribbon(aes(date, ymin = ll, ymax = ul), alpha = 0.5, fill = cols[3])+
+  geom_line(aes(date, dts), linewidth = 1)+
+  geom_line(aes(date, bsn), linewidth = 1, col = cols[3])+
   theme_bw()
 
 # excess estimation
-esp_exc <- 
-  esp_bsn %>% 
+exc <- 
+  bsn %>% 
   filter(date >= "2020-03-15",
          date <= "2022-12-31") %>% 
-  mutate(exc_all = dts - bsn, # all excess
-         exc_pos = ifelse(exc_all > 0, exc_all, 0), # only positive excess
-         exc_sig = ifelse(dts > ul, exc_all, 0)) # only positive ans significant excess
-  
-
-# comparison
-esp_exc %>%
-  select(date, starts_with("exc")) %>% 
-  gather(-date, key = type, value = excess) %>% 
-  ggplot()+
-  geom_line(aes(date, excess, col = type))+
-  geom_hline(yintercept = 0, linetype = "dashed")+
-  theme_bw()
-
-# annual difference between alternative Poisson excess estimates
-esp_exc_yr <- 
-  esp_exc %>% 
-  group_by(year) %>% 
-  summarise(exc_all = sum(exc_all),
-            exc_sig = sum(exc_sig),
-            exc_pos = sum(exc_pos)) %>% 
-  ungroup() %>% 
-  mutate(diff_abs_pos = exc_pos- exc_all,
-         diff_abs_sig = exc_sig- exc_all,
-         diff_rel_pos = diff_abs_pos / exc_all,
-         diff_rel_sig = diff_abs_sig / exc_all)
-
-esp_exc_yr
-# when excluding negative excess we add between 5% and 80% excess deaths by year
-
-esp_exc_yr %>% 
-  group_by() %>% 
-  summarise(exc_all = sum(exc_all),
-            exc_sig = sum(exc_sig),
-            exc_pos = sum(exc_pos)) %>% 
-  ungroup() %>% 
-  mutate(diff_abs_pos = exc_pos- exc_all,
-         diff_abs_sig = exc_sig- exc_all,
-         diff_rel_pos = diff_abs_pos / exc_all,
-         diff_rel_sig = diff_abs_sig / exc_all)
-
-# 18% higher excess between 2020 and 2022 when excluding negative excess 
-# estimates!! Not negligible at all
+  mutate(exc = dts - bsn) 
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -325,44 +262,48 @@ esp_exc_yr %>%
 
 # comparing excess estimation approaches ====
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-comp <- 
-  esp_yw_av %>% 
-  select(year, week, date, dts, bsn) %>% 
-  mutate(type = "weekly_average") %>% 
-  bind_rows(esp_wk_av %>% 
+
+# binding together the three estimates
+
+bsns <- 
+  bind_rows(dt_w_av %>% 
               select(year, week, date, dts, bsn) %>% 
-              mutate(type = "weekly_spc_average")) %>% 
-  bind_rows(esp_bsn %>% 
+              mutate(type = "weekly_average"),
+            dt_ws_av %>% 
+              select(year, week, date, dts, bsn) %>% 
+              mutate(type = "weekly_spc_average"),
+            bsn %>% 
               select(year, week, date, dts, bsn) %>% 
               mutate(type = "poisson_model"))
 
 # plotting the three baselines
-comp %>% 
+p_bsns <- 
+  bsns %>% 
   ggplot()+
-  geom_line(aes(date, dts))+
-  geom_line(aes(date, bsn, col = type))+
+  geom_line(aes(date, dts), linewidth = 1)+
+  geom_line(aes(date, bsn, col = type), linewidth = 1)+
+  scale_color_manual(values = cols)+
   theme_bw()
+p_bsns
 
-ggsave("figures/weekly_mortality_spain_baselines.png",
-       w = 8, h = 2)
-
-wk_comp <- 
-  comp %>% 
+excs <- 
+  bsns %>% 
   mutate(exc = dts - bsn,
          psc = dts / bsn) %>% 
   filter(date >= "2020-03-15",
          date <= "2022-12-31")
 
 # visualizing weekly excess deaths
-wk_comp %>% 
+excs %>% 
   ggplot()+
-  # geom_line(aes(date, exc))+
-  geom_line(aes(date, exc, col = type))+
+  geom_line(aes(date, exc, col = type), linewidth = 1)+
   geom_hline(yintercept = 0, linetype = "dashed")+
+  scale_color_manual(values = cols)+
   theme_bw()
 
 # visualizing cumulative excess
-wk_comp %>% 
+excs_cum <- 
+  excs %>% 
   group_by(type) %>% 
   mutate(exc_cum = cumsum(exc)) %>% 
   ggplot()+
@@ -370,36 +311,88 @@ wk_comp %>%
   geom_line(aes(date, exc_cum, col = type))+
   theme_bw()
 
-ggsave("figures/spain_weekly_cum_excess.png",
-       w = 8, h = 2)
-
 # obtaining annual excess
-yr_comp_exc <- 
-  comp %>% 
+# yr_exc <- 
+#   excs %>% 
+#   filter(date >= "2020-03-15",
+#          date <= "2022-12-31") %>% 
+#   group_by(year, type) %>% 
+#   summarise(exc = sum(exc)) %>% 
+#   ungroup() %>%
+#   spread(year, exc) %>% 
+#   mutate(total = `2020` + `2021` + `2022`)
+# 
+# yr_exc
+
+yr_exc <- 
+  excs %>% 
   filter(date >= "2020-03-15",
          date <= "2022-12-31") %>% 
-  mutate(exc = dts - bsn) %>% 
   group_by(year, type) %>% 
   summarise(exc = sum(exc)) %>% 
-  ungroup() %>%
-  spread(type, exc)
+  ungroup()
 
-yr_comp_diff <- 
-  yr_comp_exc %>% 
-  mutate(diff_abs_wa = weekly_average - poisson_model,
-         diff_rel_wa = weekly_average / poisson_model,
-         diff_abs_wsa = weekly_spc_average - poisson_model,
-         diff_rel_wsa = weekly_spc_average / poisson_model) %>% 
-  select(year, starts_with("diff"))
+yr_exc %>% 
+  spread(year, exc)
 
-tt_comp_diff <- 
-  yr_comp_exc %>% 
-  summarise(diff_abs_wa = sum(weekly_average) - sum(poisson_model),
-            diff_rel_wa = sum(weekly_average) / sum(poisson_model),
-            diff_abs_wsa = sum(weekly_spc_average) - sum(poisson_model),
-            diff_rel_wsa = sum(weekly_spc_average) / sum(poisson_model)) 
 
-tt_comp_diff
+tots <- 
+  yr_exc %>% 
+  spread(type, exc) %>% 
+  summarise(poisson_model = sum(poisson_model),
+            weekly_average = sum(weekly_average),
+            weekly_spc_average = sum(weekly_spc_average),
+            year = "total")
+
+tots
+
+t1 <- round(tots$poisson_model)
+t2 <- round(tots$weekly_average)
+t3 <- round(tots$weekly_spc_average)
+
+d2 <- paste0(round((t2 / t1 - 1)*100, 1), "%")
+d3 <- paste0(round((t3 / t1 - 1)*100, 1), "%")
+
+# plotting excess estimates by year
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+yr_exc %>% 
+  ggplot(aes(type, exc, fill = factor(year))) + 
+  geom_bar(position="stack", stat="identity")+
+  geom_text(aes(x = "poisson_model", y = 0), label = paste0(t1, "\nreference"), vjust = -1)+
+  geom_text(aes(x = "weekly_average", y = 0), label = paste0(t2, "\n", d2), vjust = -1)+
+  geom_text(aes(x = "weekly_spc_average", y = 0), label = paste0(t3, "\n", d3), vjust = -1)+
+  labs(fill = "year")+
+  coord_cartesian(expand = 0)+
+  theme_bw()
+
+
 
 # Looking at the three years 2020-2022, estimates using averages double 
 # those obtained from the Poisson model!!!
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~
+# Assignment in class: ====
+# ~~~~~~~~~~~~~~~~~~~~~~~~~
+# Estimate excess mortality in the US and France using the three methods and calculate 
+# the potential bias when using the average approach
+
+excess <- 
+  obtain_excess(cd = "USA", sx = "b", ag = "TOT", ymin = 2013)
+
+excess[[1]]
+excess[[2]]
+excess[[3]] %>% spread(type, exc)
+
+excess <- 
+  obtain_excess(cd = "FRA", sx = "b", ag = "TOT", ymin = 2013)
+
+excess[[1]]
+excess[[2]]
+excess[[3]] %>% spread(type, exc)
+
+# ok, that was quick...
+
+# have fun in Euskal Erkidegoa!!
+# thanks for all, you are a great group, it was great to give this course :)
+
